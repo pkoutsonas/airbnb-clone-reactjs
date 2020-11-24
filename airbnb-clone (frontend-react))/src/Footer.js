b2fb91acee00b8a0e6e1b6a101da338a016f334f
pkoutsonas <pkoutson@gmail.com>
2020-11-24 17:37:14 +0200

    Add files via upload

---
+++ airbnb-clone (frontend-react))/src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <h5>Made by @paul_kouts</h5>
+      <p>This is a demo</p>
+    </div>
+  );
+}
+
+export default Footer;
